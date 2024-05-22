8fdaf8b42e9b45d22e3b6765f48acaff7047786f
Javier Rivera <javierrisa@gmail.com>
2024-05-22 00:53:40 -0700

    UI of the Signup done

--- app/src/main/java/com/example/moneyroute/MainActivity.kt
+++ app/src/main/java/com/example/moneyroute/MainActivity.kt
@@ -15,29 +15,43 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.moneyroute.ui.login.ui.LoginScreen
 import com.example.moneyroute.ui.login.ui.LoginViewModel
+import com.example.moneyroute.ui.signup.ui.SignupScreen
+import com.example.moneyroute.ui.signup.ui.SignupViewModel
 import com.example.moneyroute.ui.theme.MoneyRouteTheme
 import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
     private val loginViewModel by viewModels<LoginViewModel>()
+    private val signupViewModel by viewModels<SignupViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MoneyRouteTheme {
                 Scaffold { innerPadding ->
-                    LoginScreen(modifier = Modifier.padding(innerPadding).fillMaxSize(), viewModel = loginViewModel)
+//                    LoginScreen(modifier = Modifier.padding(innerPadding).fillMaxSize(), viewModel = loginViewModel)
+                    SignupScreen(modifier = Modifier.padding(innerPadding).fillMaxSize(), viewModel = signupViewModel)
                 }
             }
         }
     }
 }
 
-@Preview(showSystemUi = true, showBackground = true)
+//@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun LoginScreenPreview() {
     MoneyRouteTheme {
         Scaffold { innerPadding ->
             LoginScreen(modifier = Modifier.padding(innerPadding).fillMaxSize(), viewModel = LoginViewModel())
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun SignupScreenPreview() {
+    MoneyRouteTheme {
+        Scaffold { innerPadding ->
+            SignupScreen(modifier = Modifier.padding(innerPadding).fillMaxSize(), viewModel = SignupViewModel())
         }
     }
 }
